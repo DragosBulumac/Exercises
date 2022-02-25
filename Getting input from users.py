b6b4b23e18f0616b4dc9c754c834fffc8f86b6da
DragosBulumac <75696927+DragosBulumac@users.noreply.github.com>
2022-02-25 20:30:48 +0200

    Add files via upload

---
+++ Getting input from users.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age : ")
+print("Hello " +name+"!You have " +age +"")
+print("input aceasta functie preia exact ce este scris in interior il converteste intr-un string ")
+
